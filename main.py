8238b3e7b709d14716f649a8ead7d5d94514fb19
ShubhamAmbre1 <shubhamambre94@gmail.com>
2020-11-18 14:13:08 +0530

    improvements

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import sqlite3
 import hashlib
 import os
+import subprocess as sp
 
-def dec(func):
+def password(func):
     def pass_checker():
         global conn,c
         conn = sqlite3.connect(path)
@@ -18,21 +19,25 @@ def dec(func):
             return pass_checker()
     return pass_checker
 
-
 def create_hider():
-
     conn = sqlite3.connect(path)
+    sp.check_call(f'attrib +H {path}')
+    print("hidding database")
+    print("Connected to the database")
     c = conn.cursor()
+    print("Test")
     c.execute("""CREATE TABLE files(
         name TEXT,
-        file BLOB NOT NULL
+        file BLOB NOT NULL,
+        destination TEXT
         )""")
+    print("Testing")
     conn.commit()
-    
+
     create_password = hashlib.sha256(input("Enter a password : ").encode('ascii')).hexdigest()
 
-    c.execute("CREATE TABLE passw(password TEXT)")   
-    conn.commit() 
+    c.execute("CREATE TABLE passw(password TEXT)")
+    conn.commit()
 
     c.execute("INSERT INTO passw(password) VALUES(:passwo)",{'passwo': create_password})
     conn.commit()
@@ -42,29 +47,36 @@ def create_hider():
     conn.close()
 
 
-@dec
+@password
 def add_file():
     file = input("Enter file path = ")
-    lis = file.split("/") 
-    
+    lis = file.split("/")
+
+    print(file , "\n", lis)
+
+    print(lis[len(lis)-1:][0])
     with open(file, 'rb') as f:
         data = f.read()
 
-    c.execute("INSERT INTO files(name, file) VALUES(:name ,:file)",{'name':lis[len(lis)-1:][0], 'file': data})
+    c.execute("INSERT INTO files(name, file, destination) VALUES(:name ,:file, :destination)",{'name':lis[len(lis)-1:][0], 'file': data, 'destination': "/".join(lis[:len(lis)-1])})
     print("File added.")
+    os.remove(file)
 
     conn.commit()
     conn.close()
-    
 
-@dec
+
+@password
 def remove_file():
     name = input("Enter the file name = ")
 
-    c.execute("SELECT name,file FROM files WHERE name = (:name)",{'name': name})
+    c.execute("SELECT name,file,destination FROM files WHERE name = (:name)",{'name': name})
 
     content = c.fetchone()
-    with open(name, 'wb') as f:
+    #create fetch path and name
+    path = content[2] + "/" + name
+
+    with open(path, 'wb') as f:
         f.write(content[1])
     conn.commit()
 
@@ -76,7 +88,7 @@ def remove_file():
     conn.close()
 
 
-@dec
+@password
 def delete_hider():
     conn.close()
     path_list = path.split("/")
@@ -107,16 +119,16 @@ example : C:/Users/Pc/Desktop/hider.db
 
 ************************************
 = """))
-    
+
         if i == 1:
             try:
                 create_hider()
             except:
                 print("Already created!")
-        
+
         elif i == 2:
             add_file()
-            
+
         elif i == 3:
             try:
                 remove_file()
